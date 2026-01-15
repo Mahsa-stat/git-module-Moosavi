7a3bed85c668da6a6581d437b379d5d8ff78b95e
Mahsa-stat <musavi.mahsa@yahoo.com>
2026-01-15 00:20:51 -0600

    added moving average calculation function

--- read_data.py
+++ read_data.py
@@ -3,18 +3,34 @@ import matplotlib.pyplot as plt
 import os
 DATA_URL = 'https://raw.githubusercontent.com/plotly/datasets/master/2014_apple_stock.csv'
 def read_and_visualize():
-    Apple_Inc = pd.read_csv(DATA_URL)
-    print(Apple_Inc)
-    count_row = Apple_Inc.shape[0]
-    count_col = Apple_Inc.shape[1]
+    df = pd.read_csv(DATA_URL)
+    print(df)
+    count_row = df.shape[0]
+    count_col = df.shape[1]
     print(count_row)
     print(count_col)
     print("Total rows:", count_row)
     print("Total columns:", count_col)
-    plt.plot(Apple_Inc['AAPL_x'], Apple_Inc['AAPL_y'])
-    plt.savefig('apple_stock.png')
+    plt.plot(df["AAPL_x"], df["AAPL_y"], label="Stock Prices")
+    plt.savefig("apple_stock.png")
     plt.close()
-    return Apple_Inc
+    return df
+def calculate_moving_average(df, window_size=30):
+    df_SMA = df["AAPL_y"].rolling(window=window_size).mean()
+    
+    plt.plot(df["AAPL_x"], df["AAPL_y"], label="Stock Prices")
+    plt.plot(df['AAPL_x'], df_SMA, label=f"SMA ({window_size})")
+    plt.legend()
+    plt.savefig("apple_stock_sma.png")
+    plt.close()
+    
+    return df
+
+#df = read_and_visualize()
+#df = calculate_moving_average(df, window_size=30)
+
+
+    
 
     
     
